410d625b855eeda27644d9285e58f4c7569a9012
Aurelia <ac2590@bath.ac.uk>
2022-01-20 19:32:08 +0000

    Create starting screen and first question

---
+++ app/src/main/java/com/example/sleepaid/InformationBox.java
@@ -0,0 +1,36 @@
+package com.example.sleepaid;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+public class InformationBox extends FrameLayout {
+    TextView text;
+
+    public InformationBox(Context context, AttributeSet attributes) {
+        super(context, attributes);
+        init(context, attributes);
+    }
+
+    private void init(Context context, AttributeSet attributes) {
+        inflate(context, R.layout.information_box, this);
+
+        int[] sets = {R.attr.text};
+        TypedArray typedArray = context.obtainStyledAttributes(attributes, sets);
+        CharSequence boxText = typedArray.getText(0);
+        typedArray.recycle();
+
+        initComponents();
+        setText(boxText);
+    }
+
+    private void initComponents() {
+        text = (TextView) findViewById(R.id.box_Text);
+    }
+
+    public void setText(CharSequence value) {
+        text.setText(value);
+    }
+}
---
+++ app/src/main/java/com/example/sleepaid/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.sleepaid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startQuestionnaire(android.view.View view) {
+        setContentView(R.layout.activity_question1);
+    }
+}
