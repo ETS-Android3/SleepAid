f27476497e9f7296b5eb5a0adc25e301b0038a2d
Aurelia <ac2590@bath.ac.uk>
2022-01-24 21:53:46 +0000

    Revert back to SQLite for now as its a bit complicated

--- app/src/main/java/com/example/sleepaid/MainActivity.java
+++ app/src/main/java/com/example/sleepaid/MainActivity.java
@@ -1,45 +1,28 @@
 package com.example.sleepaid;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
 
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    AppDatabase db;
-    LoadAnswers runningTask;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        db = Room.databaseBuilder(
-                getApplicationContext(),
-                AppDatabase.class,
-                "sleep-aid.db"
-        ).build();
+        DBHelper db = new DBHelper(this);
+        db.getWritableDatabase();
 
-        if (runningTask != null) {
-            runningTask.cancel(true);
+        Cursor answerData = db.load(SleepAidContract.SleepAidEntry.ANSWER_TABLE);
+
+        if (answerData.moveToFirst()) {
+            goToHomeScreen();
         }
-        runningTask = new LoadAnswers();
-        runningTask.execute();
-
-//        myDB = new DBHelper(this);
-//        myDB.getWritableDatabase();
-
-//        Cursor answerData = myDB.load(SleepAidContract.SleepAidEntry.ANSWER_TABLE);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // Cancel running task(s) to avoid memory leaks
-        if (runningTask != null)
-            runningTask.cancel(true);
+        else {
+            setContentView(R.layout.activity_main);
+        }
     }
 
     public void startQuestionnaire(View view) {
@@ -50,23 +33,5 @@ public class MainActivity extends AppCompatActivity {
     private void goToHomeScreen() {
         Intent homeScreen = new Intent(this, HomeScreen.class);
         startActivity(homeScreen);
-    }
-
-    private final class LoadAnswers extends AsyncTask<Void, Void, String> {
-
-        @Override
-        protected String doInBackground(Void... params) {
-            return db.answerDao().getAll().isEmpty() ? "Yes" : "No";
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            if (result == "Yes") {
-                setContentView(R.layout.activity_main);
-            }
-            else {
-                goToHomeScreen();
-            }
-        }
     }
 }
