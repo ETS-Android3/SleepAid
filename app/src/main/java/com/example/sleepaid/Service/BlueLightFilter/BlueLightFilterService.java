ba0049ae4d732d1f1b799071e976d763d3f1eb7c
Aurelia <ac2590@bath.ac.uk>
2022-04-03 22:16:38 +0100

    Detach blue light filter service so it can run after app sleeps

--- app/src/main/java/com/example/sleepaid/Service/BlueLightFilter/BlueLightFilterService.java
+++ app/src/main/java/com/example/sleepaid/Service/BlueLightFilter/BlueLightFilterService.java
@@ -63,6 +63,7 @@ public class BlueLightFilterService extends Service {
                 .build();
 
         startForeground((int) System.currentTimeMillis(), notification);
+        stopForeground(STOP_FOREGROUND_DETACH);
         isRunning = true;
 
         return START_STICKY;
